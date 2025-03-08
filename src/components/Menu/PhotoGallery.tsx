7fc0a7513f7440acdfe12ed1af47a0ab139ead1d
aghopianm <aghopianm@gmail.com>
2025-03-08 23:08:31 +0100

    added in memo for loading images to improve performance

--- src/components/Menu/PhotoGallery.tsx
+++ src/components/Menu/PhotoGallery.tsx
@@ -1,4 +1,4 @@
-
+import { memo } from 'react';
 import { PhotoGrid, Photo } from "./styles";
 import { categoryContent } from "../data/menuData";
 import { Category } from "./index";
@@ -7,12 +7,13 @@ type PhotoGalleryProps = {
   category: Category;
 }
 
-const PhotoGallery = ({ category } :PhotoGalleryProps) => {
+const PhotoGallery = memo(({ category }: PhotoGalleryProps) => {
   if (!(category in categoryContent)) {
     return null;
-  } const photos = categoryContent[category].photos;
+  }
   
-  // Then check if there are any photos
+  const photos = categoryContent[category].photos;
+  
   if (photos.length === 0) {
     return null;
   }
@@ -21,13 +22,17 @@ const PhotoGallery = ({ category } :PhotoGalleryProps) => {
     <PhotoGrid>
       {photos.map((photo, index) => (
         <Photo
-          key={index}
+          key={`${category}-${index}`}
           src={photo}
           alt={`${category} example ${index + 1}`}
+          loading="lazy"
+          decoding="async"
         />
       ))}
     </PhotoGrid>
   );
-};
+});
+
+PhotoGallery.displayName = 'PhotoGallery';
 
 export default PhotoGallery;
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { memo, useMemo } from 'react';
 import { useSelector } from "react-redux";
 import type { RootState } from "../../store/store";
 import type { Product } from "../../types/types";
@@ -12,14 +13,14 @@ import { MenuContainer, Title } from "./styles";
 
 export type Category = "weddings" | "birthdays" | "cupcakes" | "prices";
 
-const Menu = () => {
-  // Remove useState and use Redux state instead
+const Menu = memo(() => {
   const activeCategory = useSelector((state: RootState) => state.menu.activeCategory);
   const { items, loading } = useSelector((state: RootState) => state.products);
 
-  const filteredProducts = items.filter((product: Product) => {
-    return product.category === activeCategory;
-  });
+  const filteredProducts = useMemo(() => 
+    items.filter((product: Product) => product.category === activeCategory),
+    [items, activeCategory]
+  );
 
   return (
     <>
@@ -43,6 +44,8 @@ const Menu = () => {
       <Footer />
     </>
   );
-};
+});
+
+Menu.displayName = 'Menu';
 
 export default Menu;
